faf23a257d9939b8382db25f7842f9ac9aebf136
univium <85970292+univium@users.noreply.github.com>
2026-01-01 19:39:08 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -66,7 +66,7 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.OxHugoFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
